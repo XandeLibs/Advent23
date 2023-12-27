1da942c840ee616e284aa2e89cbd770576758dbb
Alexandre Libório da Fonseca <github.igloo087@passinbox.com>
2023-12-26 23:56:40 -0400

    adds solved day 6

---
+++ Day 6/code.cpp
@@ -0,0 +1,85 @@
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#define DEBUG
+
+#ifdef DEBUG
+#define dbgWrite( str ) do { std::cout << str << std::endl; } while(false)
+#else
+#define dbgWrite( str ) do { } while(false)
+#endif // DEBUG
+
+using vector = std::vector<uint>;
+
+int main (int argc, char *argv[]) {
+  std::ifstream input(argv[1]);
+  std::ofstream output(argv[2]);
+  std::istringstream ss;
+  std::string line, word;
+  vector time, distance;
+  int result = 1;
+
+  // Receiving input -------------------------
+  std::getline(input, line);
+  ss.str(line);
+  ss >> word; // Word "Time: "
+
+  while(ss >> word){
+    time.push_back(atoi(word.c_str()));
+  }
+
+  ss.clear();
+
+  std::getline(input, line);
+  ss.str(line);
+  ss >> word; // Word "Distance: "
+
+  while(ss >> word){
+    distance.push_back(atoi(word.c_str()));
+  }
+  //----------------------------------------
+
+  // p = time pressed, t = race time
+  // Distance = p*(t-p)
+  // -p^2 + pt - d = 0
+  // p^2 - pt + d = 0
+  // t is known, distance to beat is known, p can be found
+  // sum = -b/a, sum = t
+  // prod = c/a, prod = d
+  // 
+
+  auto distance_iter = distance.begin();
+  uint middle, diff;
+  for(auto t: time){
+    auto d = *(distance_iter++);
+
+    dbgWrite("Testing time " << t << " for record " << d);
+
+    if(t%2 == 0){
+      middle = (t/2)*(t/2);
+
+      diff = ceil(sqrt(middle - d))-1;
+
+      dbgWrite("T even, middle " << middle << " diff " << diff << " adding to result " << diff*2 + 1);
+      
+      result *= diff*2 + 1;
+    }
+    else {
+      middle = (t/2)*(t/2 + 1);
+
+      diff = ceil(sqrt(middle - d))-1;
+
+      dbgWrite("T even, middle " << middle << " diff " << diff << " adding to result " << (diff+1)*2);
+
+      result *= (diff+1)*2;
+    }
+  }
+
+  output << result << '\n';
+
+  return 0;
+}
