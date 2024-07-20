c94eae4220683a1cbc3abc448a8db93dd3f0c5a7
XandeLibs <github.igloo087@passinbox.com>
2024-07-19 23:51:55 -0400

    atualiza arquivos

--- Day 5-1 GPU/code.cu
+++ Day 5-1 GPU/code.cu
@@ -17,6 +17,8 @@ using vector2 = std::vector<array2>;
 using vector3 = std::vector<array3>;
 using almanac = std::array<std::array<vector, 3>, 8>;
 
+const int Nthreads = 64;
+
 enum almanac_list {
   seed,
   soil,
@@ -28,7 +30,10 @@ enum almanac_list {
   loc
 };
 
-const auto almanac_size = loc+1;
+const auto almanac_size = 7;
+const auto MAP_MAX_SIZE = Nthreads;
+const auto data_size = sizeof(data);    
+const auto height_bytes = 47*data_size;
 
 std::string almanac_names[loc+1] = {
   "seed",
@@ -41,12 +46,9 @@ std::string almanac_names[loc+1] = {
   "loc"
 };
 
-__global__ void minConvertedSeed_GPU(std::ifstream &input, const almanac &alm);
+__device__ data g_alm[7][MAP_MAX_SIZE], g_alm_dst[7][MAP_MAX_SIZE][2];
 
-vector2 convert(const vector2 &source, 
-                const vector &src_start, 
-                const vector &dst_start, 
-                const vector &range);
+__global__ void minConvertedSeed_GPU(array2* seed_list);
 
 void buildMap(std::ifstream &input, 
               vector &src_start, 
@@ -54,6 +56,8 @@ void buildMap(std::ifstream &input,
               vector &range);
 
 void print_vector(std::string message, vector2 vec);
+
+void alloc_almanac(almanac &alm, data (&g_map)[MAP_MAX_SIZE], data (&g_map_rngdst)[MAP_MAX_SIZE][2], int index);
 
 int main (int argc, char *argv[]) {
   auto start_clock = std::chrono::system_clock::now();
@@ -65,6 +69,7 @@ int main (int argc, char *argv[]) {
   long long result = 0;
   array2 seed_range;
   vector2 seed_list;
+  long long unsigned int seed_range_temp;
   // 8 maps, each with 3 vectors(source_start, destination_start and range)
   almanac alm;
 
@@ -82,176 +87,32 @@ int main (int argc, char *argv[]) {
   }
   // ------------------------------------------
 
-  print_vector("Seeds: ", seed_list);
-
   std::getline(input, line); // Empty line
+  
+  cudaMalloc((void**)&g_alm, data_size * 7 * MAP_MAX_SIZE);
+  cudaMalloc((void**)&g_alm_dst, data_size * 7 * MAP_MAX_SIZE * 2);
 
-  // Reads and sorts all the maps
+  // Reads and sorts all the maps, async copies to gpu
   for(auto i: {0, almanac_size-1}){
     buildMap(input, alm[i][0], alm[i][1], alm[i][2]);
+    alloc_almanac(alm, g_alm[i], g_alm_dst[i], i);
+    // 14 total cudaMemcpy
   }
 
-  // Copying data to GPU
-  auto data_size = sizeof(data);
-  auto seed_size = seed_list.size();
-  size_t alm_size[almanac_size];
-
-  for (auto i : {0, almanac_size-1})
-    alm_size[i] = alm[i].size();
-
-  data* g_seed_list;
-  // Almanac, each array has different sizes, decided to do various 2D arrays instead of a big 3D one
-  data* g_seed, *g_soil, *g_fert, *g_water, *g_light, *g_temp, *g_humid, *g_loc;
-  data* g_seed_dst, *g_soil_dst, *g_fert_dst, *g_water_dst, *g_light_dst, *g_temp_dst, *g_humid_dst, *g_loc_dst;
-
-  auto alloc_almanac = [alm, data_size](data* g_map, data* g_map_dst, int index){
-    auto map_array = new data[alm[index][0].size()][2]; // Array with source and range
-    auto height_bytes = alm[index][0].size()*data_size;
-
-    memcpy(map_array[0], alm[index][0].data(), height_bytes); // Copy source
-    memcpy(map_array[1], alm[index][2].data(), height_bytes); // Copy range
-
-    cudaMemcpy2D(g_map, height_bytes, map_array, height_bytes, height_bytes, 2*data_size, cudaMemcpyDefault);
-
-    // Copies dest_range
-    cudaMemcpy(g_map_dst, alm[index][1].data(), height_bytes, cudaMemcpyDefault);
-  };
-
-  //cudaMemcpy(g_seed_list, seed_list.data(), seed_size*data_size*2, cudaMemcpyDefault);
-  // tem que enviar só o qnt cada thread vai precisar, já que todas são iguais exceto uma
-  alloc_almanac(g_seed, g_seed_dst, 0);
-  alloc_almanac(g_soil, g_soil_dst, 1);
-  alloc_almanac(g_fert, g_fert_dst, 2);
-  alloc_almanac(g_water, g_water_dst, 3);
-  alloc_almanac(g_light, g_light_dst, 4);
-  alloc_almanac(g_temp, g_temp_dst, 5);
-  alloc_almanac(g_humid, g_humid_dst, 6);
-  alloc_almanac(g_loc, g_loc_dst, 7);
-  // 17 total cudaMemcpy
-
+  minConvertedSeed_GPU<<<Nthreads, seed_list.size()>>>(seed_list.data());
   // -------------------
+
+  cudaFree(g_alm);
+  cudaFree(g_alm_dst);
 
   output << result << '\n';
 
   auto end_clock = std::chrono::system_clock::now();
   auto time_elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end_clock - start_clock).count();
 
-
   std::cout << "\nTime to execute: " << time_elapsed << " nanoseconds\n";
 
   return 0;
-}
-
-vector2 mapConvert(std::ifstream &input, const vector2 &source){
-  vector source_range, dest_range, range;
-  vector2 destination;
-
-  buildMap(input, source_range, dest_range, range);
-
-  destination = convert(source, source_range, dest_range, range);
-
-  return destination;
-
-}
-
-vector2 convert(const vector2 &source, const vector &map_src_start, const vector &map_dst_start, const vector &map_range){
-  vector2 destination;
-  long long pos, offset, p_range_end;
-  array2 partial_range;
-  auto& p_range_start = partial_range[0];
-  auto& p_range_value = partial_range[1];
-
-  // Conversion occurs in ranges
-  // If the start and the end have differente upper bounds, a split is guaranteed
-  // It just needs to find the split point
-  // If they have the same upper bounds, a split may not happen
-  // A check for if both are inside the map range needs to be done
-  for(auto& [s, source_range]: source){
-
-    p_range_start = s;
-    p_range_value = source_range;
-    p_range_end = p_range_start + p_range_value - 1;
-
-  NEXT_PARTIAL_RANGE: 
-    p_range_end = p_range_start + p_range_value - 1;
-    // First value larger than s, if none, returns last
-    auto bound_str = std::upper_bound(map_src_start.begin(), map_src_start.end(), p_range_start);
-    auto bound_end = std::upper_bound(map_src_start.begin(), map_src_start.end(), p_range_end);
-
-    auto split = [&](auto range_value, bool inside_map){
-      array2 new_split;
-
-      if(inside_map){
-        new_split[0] = map_dst_start[pos] + offset;
-        new_split[1] = map_range[pos] - offset;
-      }
-      else{
-        new_split[0] = p_range_start;
-        new_split[1] = range_value;
-      }
-
-      p_range_start = p_range_start + range_value;
-      p_range_value = p_range_value - range_value;
-
-      destination.push_back(new_split);
-    };
-
-    // Split is guaranteed 
-    if(bound_str != bound_end){
-      // Out of map before first range
-      if(bound_str == map_src_start.begin()){
-        split(*map_src_start.begin() - p_range_start, false);
-        goto NEXT_PARTIAL_RANGE;
-      }
-
-      bound_str--;
-      pos = std::distance(map_src_start.begin(), bound_str);
-      offset = p_range_start - *bound_str;
-
-      // Inside map in bound_str range
-      if(p_range_start < *bound_str + map_range[pos]-1){
-        split(*bound_str + map_range[pos] - p_range_start, true);
-        goto NEXT_PARTIAL_RANGE;
-      }
-
-      // Outside map after bound_str range
-      bound_str++;
-      split(*bound_str - p_range_start, false);
-      goto NEXT_PARTIAL_RANGE;
-    }
-
-    // Same upper bounds
-    // Both out of map
-    if(bound_str == map_src_start.begin()){
-      destination.push_back(partial_range);
-      continue;
-    }
-
-    bound_str--;
-    bound_end--;
-
-    pos = std::distance(map_src_start.begin(), bound_str);
-    offset = p_range_start - *bound_str;
-
-    // Start is inside map
-    if(p_range_start < *bound_str + map_range[pos]-1){
-      // Both inside
-      if(p_range_end < *bound_str + map_range[pos]-1){
-        p_range_start = map_dst_start[pos] + offset;
-        destination.push_back(partial_range);
-      }
-      else{
-        split(*bound_str + map_range[pos] - p_range_start, true);
-        goto NEXT_PARTIAL_RANGE;
-      }
-    }
-    // Both outside map
-    else{
-      destination.push_back(partial_range);
-    }
-  }
-
-  return destination;
 }
 
 void buildMap(std::ifstream &input, vector &source_range, vector &dest_range, vector &range){
@@ -271,9 +132,14 @@ void buildMap(std::ifstream &input, vector &source_range, vector &dest_range, ve
       map_line[i] = atoll(word.c_str());
     }
 
+    // source now has end of range
+    map_line[0] += map_line[2];
+
     SDR.push_back(map_line); // Fills SDR with source, dest and range numbers
     std::getline(input, line);
   }
+
+  std::fill_n(SDR.end(), MAP_MAX_SIZE - SDR.size(), std::array<data, 3> {UINT64_MAX, UINT64_MAX, UINT64_MAX});
 
   auto sortSDR = [](array3 va, array3 vb) { return (va[1] < vb[1]); };
 
@@ -287,6 +153,105 @@ void buildMap(std::ifstream &input, vector &source_range, vector &dest_range, ve
 
 }
 
-__global__ void minConvertedSeed_GPU(std::ifstream &input, const almanac &alm){
+inline void alloc_almanac(almanac &alm, data (&g_map)[MAP_MAX_SIZE], data (&g_map_rngdst)[MAP_MAX_SIZE][2], int index){
+    auto map_array = new data[alm[index][0].size()][2]; // Array with range and dest
+
+    memcpy(map_array[0], alm[index][2].data(), height_bytes); // Copy range
+    memcpy(map_array[1], alm[index][1].data(), height_bytes); // Copy dest
+
+    cudaMemcpyToSymbolAsync(g_map, alm[index][0].data(), height_bytes);
+    cudaMemcpyToSymbolAsync(g_map_rngdst, map_array, 2*height_bytes);
+}
+
+__global__ void minConvertedSeed_GPU(array2* seed_list){
+  unsigned int tid = threadIdx.x;
+  unsigned int bid = blockIdx.x;
+  int offset = MAP_MAX_SIZE/2;
+  unsigned int found_range[7];
+
+  // copy maps to shared memory
+  __shared__ data sh_g_alm[7][MAP_MAX_SIZE], sh_g_alm_dst[7][MAP_MAX_SIZE][2];
   
+  for (auto i=0; i < 7; i++)
+  {
+    sh_g_alm[i][tid] = g_alm[i][tid];
+  }
+
+  for (auto i = 0; i < 7; i++)
+  {
+    sh_g_alm_dst[i][tid/2][tid%2] = g_alm_dst[i][tid/2][tid%2];
+
+    tid += offset;
+    sh_g_alm_dst[i][tid/2][tid%2] = g_alm_dst[i][tid/2][tid%2];
+    tid -= offset;
+  }
+  
+  __syncthreads();
+
+  // calculate which interval and offset the thread will use
+  auto seed_range = seed_list[bid];
+  auto seed_amount = seed_range[1]/Nthreads;
+  auto seed_start = seed_range[0] + (tid * seed_amount);
+  auto seed_end = seed_start + seed_amount;
+
+  if(tid == Nthreads-1){ // last thread
+    seed_end = seed_range[1];
+    seed_amount = seed_range[1] - seed_start;
+  }
+
+  // starts converting
+
+  //first instance, creates found range
+
+  /*começa com a lista vazia, popula uma vez,
+  se nada mudar, continua usando o mesmo indice
+  quando falhar na checagem inicial, os proximos serao zerados,
+  talvez usar um goto para a parte de popular?
+  se nao teriamos q ficar checando e manipulando o found_range*/
+
+  auto upper_bound = [](const data (&first)[MAP_MAX_SIZE], const data value){
+    auto f_index = 0;
+    auto l_index = MAP_MAX_SIZE;
+    data it;
+    auto count = 64;
+    int step;
+
+    while(count > 0){
+      it = first[f_index];
+      step = count / 2;
+      f_index += step;
+
+      if(!(value < it)){
+        
+      }
+    }
+
+    return f_index;
+  };
+
+  for(auto seed = seed_start; seed <= seed_end; seed++){ // seed by seed, brute force method
+    for(auto map_n = 0; map_n < 7; map_n++){             // for each map, seed to soil etc
+      auto& g_src_end = g_alm[map_n][found_range[map_n]];
+      auto& g_range = g_alm_dst[map_n][found_range[map_n]][0];
+      auto& g_dest = g_alm_dst[map_n][found_range[map_n]][1];
+
+      if(seed < g_src_end){
+        seed = g_dest;
+      }
+      else
+      {
+        found_range[map_n]++;
+        // Higher than start of the next interval, aka inside the next interval
+        if(seed > g_src_end - g_range){
+          seed = g_dest;
+        }
+        
+        for(auto map_o = map_n; map_o < 7; map_o++){
+          auto sdasd = (g_alm[map_n], g_alm[map_n], seed);
+        }
+        break;
+      }
+      
+    }
+  }    
 }
