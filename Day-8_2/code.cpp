48bd9621a543ec2f34a7c40a4f0345c415ff642e
Alexandre Libório da Fonseca <github.igloo087@passinbox.com>
2024-08-09 21:19:38 -0400

    Adds part two of day 8

---
+++ Day-8_2/code.cpp
@@ -0,0 +1,74 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <ranges>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using string = std::string;
+using vecint = std::vector<int>;
+
+bool zeroes = false;
+
+vecint last_nums;
+
+vecint getDiffs(vecint &vec) {
+  vecint next_line;
+  auto num_prev = vec[0];
+  zeroes = true;
+
+  last_nums.push_back(num_prev);
+
+  for (auto &num : vec | std::views::drop(1)) {
+    next_line.push_back(num - num_prev);
+
+    if ((num - num_prev) != 0)
+      zeroes = false;
+
+    num_prev = num;
+  }
+
+  return next_line;
+}
+
+int main(int argc, char *argv[]) {
+  std::ifstream input(argv[1]);
+  std::ofstream output(argv[2]);
+  std::istringstream ss;
+  string line, word;
+  vecint sequence;
+  int result = 0;
+
+  while (std::getline(input, line)) {
+    ss.str(line);
+    zeroes = false;
+
+    while (!ss.eof()) {
+      ss >> word;
+      sequence.push_back(atoi(word.c_str()));
+    }
+
+    auto &vec = sequence;
+    while (!zeroes) {
+      vec = getDiffs(vec);
+    }
+
+    for (size_t i = 1; i < last_nums.size(); i+=2)
+    {
+      last_nums[i] *= -1;
+    }
+
+    result = std::accumulate(last_nums.begin(), last_nums.end(), result);
+
+    last_nums.clear();
+    sequence.clear();
+
+    ss.clear();
+  }
+
+  output << result << '\n';
+
+  return 0;
+}
